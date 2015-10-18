2924186b9093ad9db14f5f0fe018a0f02b5a0141
jacky ma <jimmy_blackjack@hotmail.com>
2015-10-18 06:46:03 -0700

    rsa

--- src/vpn/vpn.java
+++ src/vpn/vpn.java
@@ -5,22 +5,29 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.SecureRandom;
 import java.util.Scanner;
 
 class VPN {
-	public static Scanner input = new Scanner(System.in);
-		
+	private static Scanner input = new Scanner(System.in);
+	private BigInteger n;
+	private static BigInteger d;
+	private static BigInteger e;
+	private int bitlen = 1024;
+
+
 	public static void main(String args[]) throws Exception {
 		int mode;
-		 
-	    System.out.println("Choose mode: 1 for Server, 2 for Client");
-	    mode = input.nextInt();
+		
+		System.out.println("Choose mode: 1 for Server, 2 for Client");
+		mode = input.nextInt();
 
-	    chooseMode(mode);
+		chooseMode(mode);
 	}
-	
+
 	private static void chooseMode(int mode) throws Exception {
 		if (mode == 1) {
 			startServer();
@@ -29,8 +36,10 @@ class VPN {
 			startClient();
 		}
 	}
-	
+
 	private static void startServer() throws Exception {
+		RSA rsa = new RSA(1024);
+		
 		ServerSocket ss = new ServerSocket(1234);
 		System.out.println("Waiting");
 		Socket s = ss.accept();
@@ -38,29 +47,89 @@ class VPN {
 		OutputStream os = s.getOutputStream();
 		PrintStream ps = new PrintStream(os);
 		String message = "Sup";
-		String message2 = "Peace out";
-		ps.println(message);
+		BigInteger plaintext = new BigInteger(message.getBytes());
+	    BigInteger ciphertext = rsa.encrypt(plaintext);
+		String message2 = new String(ciphertext.toByteArray());
 		ps.println(message2);
 		ps.close();
 		ss.close();
 		s.close();
 	}
-	
+
 	private static void startClient() throws Exception {
 		int port;
-		
-	    System.out.println("Enter a port");
-	    port = input.nextInt();
-		
+		RSA rsa = new RSA(1024);
+
+		System.out.println("Enter a port");
+		port = input.nextInt();
+
 		Socket s = new Socket("localhost", port);
 		InputStream is = s.getInputStream();
 		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		String message;
-		while((message = br.readLine())!= null) {
+		String ciphertext;
+		while((ciphertext = br.readLine())!= null) {
+			BigInteger plaintext = rsa.decrypt(new BigInteger(ciphertext.getBytes()));
+		    String message = new String(plaintext.toByteArray());
 			System.out.println(message);
 		}
 		br.close();
 		s.close();
 		input.close();
+	}
+
+	public void RSA(BigInteger pubKey, BigInteger exponent) {
+		n = pubKey;
+		e = exponent;
+	}
+
+	public void RSA(int bits) {
+		bitlen = bits;
+		SecureRandom r = new SecureRandom();
+		BigInteger p = new BigInteger(bitlen / 2, 100, r);
+		BigInteger q = new BigInteger(bitlen / 2, 100, r);
+		n = p.multiply(q);
+		BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+		e = new BigInteger("3");
+		while (m.gcd(e).intValue() > 1) {
+			e = e.add(new BigInteger("2"));
+		}
+		d = e.modInverse(m);
+	}
+
+	public String encrypt(String message) {
+		return (new BigInteger(message.getBytes())).modPow(e, n).toString();
+	}
+
+	public BigInteger encrypt(BigInteger message) {
+		return message.modPow(e, n);
+	}
+
+	public String decrypt(String message) {
+		return new String((new BigInteger(message)).modPow(d, n).toByteArray());
+	}
+
+	public BigInteger decrypt(BigInteger message) {
+		return message.modPow(d, n);
+	}
+
+	public void generateKeys() {
+		SecureRandom r = new SecureRandom();
+		BigInteger p = new BigInteger(bitlen / 2, 100, r);
+		BigInteger q = new BigInteger(bitlen / 2, 100, r);
+		n = p.multiply(q);
+		BigInteger m = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+		e = new BigInteger("3");
+		while (m.gcd(e).intValue() > 1) {
+			e = e.add(new BigInteger("2"));
+		}
+		d = e.modInverse(m);
+	}
+
+	public BigInteger getN() {
+		return n;
+	}
+
+	public BigInteger getE() {
+		return e;
 	}
 } 
