b0b5548fe01f9051afbc837d7ae8125f09087460
Kerby Chang <kerbychang@gmail.com>
2015-10-19 19:09:48 -0700

    signing

--- src/vpn/vpn.java
+++ src/vpn/vpn.java
@@ -8,7 +8,10 @@ import java.io.PrintStream;
 import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyPair;
 import java.util.Scanner;
+
+import vpn.Authentication.InstanceType;
 
 class VPN {
 	private static Scanner input = new Scanner(System.in);
@@ -34,6 +37,12 @@ class VPN {
 	private static void startServer() throws Exception {
 		RSA rsa = new RSA(1024);
 		
+		Authentication auth = new Authentication(InstanceType.Server, "localhost", 1234, "sharedsecretabcdef");
+		System.out.println("The symmetric key is: " + auth.calcSymmetricKey());
+		KeyPair kp = auth.calcAsymmetricKey();
+		System.out.println("The public key is: " + kp.getPublic().toString());
+		System.out.println("The private key is: " + kp.getPrivate().toString());
+		
 		ServerSocket ss = new ServerSocket(1234);
 		System.out.println("Waiting");
 		Socket s = ss.accept();
@@ -44,6 +53,27 @@ class VPN {
 		BigInteger plaintext = new BigInteger(message.getBytes());
 	    BigInteger ciphertext = rsa.encrypt(plaintext);
 		String message2 = new String(ciphertext.toByteArray());
+		
+		System.out.println("Preparing to send data: " + "\"" + message + "\"");
+		
+		/* TEST */
+		auth.setOutputStream(os);
+		//auth.sendPublicKey();
+		// encrypt data
+		System.out.println("Encrypting " + "\"" + message + "\"...");
+		byte[] encryptedData = auth.encryptText(message);
+		System.out.println("Encrypted result is: " + encryptedData);
+		
+		// sign data
+		System.out.println("Signing encrypted data...");
+		byte[] signedEncryptedData = auth.sign(encryptedData);
+		System.out.println("Signed result is: " + signedEncryptedData);
+		
+		// send data
+		auth.sendMessage(signedEncryptedData);
+		System.out.println("Sending signed encrypted data: " + signedEncryptedData);
+		/* END TEST */
+		
 		ps.println(message2);
 		ps.close();
 		ss.close();
@@ -56,6 +86,8 @@ class VPN {
 
 		System.out.println("Enter a port");
 		port = input.nextInt();
+		
+		Authentication auth = new Authentication(InstanceType.Client, "localhost", port, "sharedsecretabcdef");
 
 		Socket s = new Socket("localhost", port);
 		ObjectInputStream is = new ObjectInputStream(s.getInputStream());
@@ -66,6 +98,37 @@ class VPN {
 		    String message = new String(plaintext.toByteArray());
 			System.out.println(message);
 		}
+		
+		/* TEST */
+		auth.setInputStream(is);
+		//auth.receivePublicKey();
+		byte[] signedEncryptedData = auth.getReceivedBytes();
+		byte[] encryptedData = new byte[0];
+		String message = "";
+		
+		System.out.println("Preparing to unsign " + auth.getReceivedBytes());
+		// unsign encrypted data
+		System.out.println("Unsigning " + signedEncryptedData + "...");
+		try {
+			encryptedData = auth.unsign(signedEncryptedData);
+			System.out.println("Unsigned result is: " + encryptedData);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.toString());
+		}
+		// decrypt data
+		System.out.println("Decrypting " + encryptedData + "...");
+		try {
+			encryptedData = auth.unsign(signedEncryptedData);
+			message = auth.decryptText(encryptedData);
+			System.out.println("Decrypting encrypted data to be: " + "\"" + message + "\"");
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.toString());
+		}
+		/* END TEST */
+		
 		br.close();
 		s.close();
 		input.close();
