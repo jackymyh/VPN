810a0f32dffb998db7898f8520b577d1909c16e7
g1-DT <dhttubc@gmail.com>
2015-10-21 22:24:04 -0700

    Andy added the MAC

--- src/vpn/aes.java
+++ src/vpn/aes.java
@@ -1,5 +1,6 @@
 package vpn;
 
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
@@ -7,6 +8,7 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.Mac;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -24,6 +26,27 @@ public class aes {
 		this.encryptKey = key;
 		
 	}
+	public String computemac (String text, String key){
+
+		try {
+			Mac mac = Mac.getInstance("HmacMD5");
+			SecretKeySpec secretkey = new SecretKeySpec(key.getBytes(),"AES");
+			mac.init(secretkey);
+			mac.update(new Base64().decode(text));
+			String macmessage = new Base64().encodeAsString((mac.doFinal()));
+			return macmessage;
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKeyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	public String encrypt(String text){
 		
 		try {
@@ -35,7 +58,8 @@ public class aes {
 		cipher.init(Cipher.ENCRYPT_MODE, secretkey);
 		String encrypted = new Base64().encodeAsString(cipher.doFinal(text.getBytes()));
 	//	System.out.println("This is the encryption:" + encrypted);
-		return encrypted;
+		String macmess = computemac(encrypted,encryptKey);
+		return encrypted+macmess;
 			
 			
 		} catch (InvalidKeyException e) {
@@ -59,13 +83,20 @@ public class aes {
 	
 	public String decrypt(String encrypted){
 		try {
-			//System.out.println("This is the test:" + encrypted);;
+			String ciphertext = encrypted.substring(0, encrypted.length()-25);
+			String ciphermac = encrypted.substring(encrypted.length()-24,encrypted.length());
 			Key key = new SecretKeySpec(this.encryptKey.getBytes(), "AES");
 			Cipher cipher = Cipher.getInstance("AES");
 			cipher.init(Cipher.DECRYPT_MODE, key);
-			byte[] decrypted = cipher.doFinal(new Base64().decode(encrypted));
-			String decrypt = new String(decrypted);
+			if(ciphermac.equals(computemac(ciphertext,encryptKey))){
+				byte[] decrypted = cipher.doFinal(new Base64().decode(encrypted));
+				String decrypt = new String(decrypted);
 			return decrypt;
+			}else {
+				System.out.println("Message has been compromised, shutting down");
+				return null;
+			}
+
 		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
