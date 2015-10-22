b30fd0bfc350472eca346128c935b71c9eebafcf
Jacky Ma <jimmy_blackjack@hotmail.com>
2015-10-21 21:01:13 -0700

    final 3

--- src/vpn/client.java
+++ src/vpn/client.java
@@ -21,8 +21,11 @@ class Client {
         
         if (MutualAuthentication.muAuth(TwoWayVPN.CLIENT, out, in, AES)) {
         	while(true){
-        	System.out.println("Data to send:");
+        	System.out.println("Data to send: (enter exit to quit)");
+      
         	String send = input.next();
+        	if (send.equals("exit"))
+        		break;
         	String encryptSend = AES.encrypt(send);
         	out.writeObject(encryptSend);
         	System.out.println("To Server>" + send);
